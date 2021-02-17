088558aa76fb5f17a2cb47de3b4c087d9ea143cc
fatmanurDemir <faatmaanurdemir@gmail.com>
2021-02-17 16:33:02 +0300

    project completed

---
+++ frmMusteriler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace restaurant
+{
+    public partial class frmMusteriler : Form
+    {
+        public frmMusteriler()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMusteriler_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
